0bc11a5103c40eafc4f55b4baee601bb196ad161
S-Laya-Yadav <layayadav278@gmail.com>
2026-02-27 12:13:06 +0530

    Updated labsheet12a and added labsheet12b

--- labsheet12a.py
+++ labsheet12a.py
@@ -1,19 +1,31 @@
 # find the minimum element from unsorted array
-def find_min(arr):
-    if not arr:  
-        return None
-    minimum = arr[0]
-    for num in arr:
-        if num < minimum:
-            minimum = num
-    return minimum
-n = int(input("Enter the number of elements: "))
-arr = []
+# def find_min(arr):
+#     if not arr:  
+#         return None
+#     minimum = arr[0]
+#     for num in arr:
+#         if num < minimum:
+#             minimum = num
+#     return minimum
+# n = int(input("Enter the number of elements: "))
+# arr = []
 
-print("Enter the elements:")
-for i in range(n):
-    element = int(input())
-    arr.append(element)
+# print("Enter the elements:")
+# for i in range(n):
+#     element = int(input())
+#     arr.append(element)
 
-print("Your array:", arr)
-print("Minimum element:", find_min(arr))
+# print("Your array:", arr)
+# print("Minimum element:", find_min(arr))
+
+
+n=int(input())
+
+arr= list(map(int, input().split()))
+
+minimun = arr[0]
+
+for x in arr:
+    if x<minimun:
+        minimun = x
+print(minimun)
